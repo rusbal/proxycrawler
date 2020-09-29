d074bb38a9f38929acd3898426a174e10d141b49
Raymond Usbal <raymond@philippinedev.com>
2020-09-30 00:38:40 +0800

    allow empty rating

--- app/models/amazon_product.rb
+++ app/models/amazon_product.rb
@@ -1,6 +1,5 @@
 class AmazonProduct < ApplicationRecord
   validates :amazon_asin, presence: true, uniqueness: true
   validates :name, presence: true
-  validates :price, presence: true
-  validates :rating, numericality: { greater_than_or_equal_to: 1.0, less_than_or_equal_to: 5.0 }
+  validates :rating, numericality: { greater_than_or_equal_to: 1.0, less_than_or_equal_to: 5.0 }, allow_nil: true
 end

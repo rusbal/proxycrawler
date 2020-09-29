12a3edec7ac6eb3a3204636552ef7ea428856b64
Raymond Usbal <raymond@philippinedev.com>
2020-09-29 12:14:34 +0800

    set request_urls as unique

--- spec/models/request_url_spec.rb
+++ spec/models/request_url_spec.rb
@@ -3,5 +3,14 @@ require 'rails_helper'
 RSpec.describe RequestUrl, type: :model do
   describe "validations" do
     it { is_expected.to validate_presence_of :url }
+
+    it "validates uniqueness of url" do
+      url = "http://www.example.com"
+      create(:request_url, url: url)
+
+      dup = build(:request_url, url: url)
+      dup.valid?
+      expect(dup.errors.messages[:url].first).to eq("has already been taken")
+    end
   end
 end

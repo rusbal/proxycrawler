4da7740dc6d052f7585d0861695aada6f53896cc
Raymond Usbal <raymond@philippinedev.com>
2020-09-29 12:16:57 +0800

    set request_url factory for unique url

--- spec/factories/request_urls.rb
+++ spec/factories/request_urls.rb
@@ -1,7 +1,5 @@
 FactoryBot.define do
   factory :request_url do
-    url { "MyString" }
-    requested_at { "2020-09-29 11:21:21" }
-    crawled_at { "2020-09-29 11:21:21" }
+    sequence(:url) { |n| "http://www.example.org/#{n}" }
   end
 end

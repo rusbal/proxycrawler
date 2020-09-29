c4a5b30dd4e6572f78406ab7cd7d4a438f39d2cc
Raymond Usbal <raymond@philippinedev.com>
2020-09-30 00:38:13 +0800

    display_price

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,9 @@
 module ProductsHelper
+  def display_price(product)
+    price = "#{product.price_symbol}#{product.price}"
+    if product.price_crossed_out && product.price_crossed_out > 0.0
+      price += " <strike>#{product.price_crossed_out}</strike>"
+    end
+    price.html_safe
+  end
 end

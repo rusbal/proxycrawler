b7ed82b143b59e2e8958025e64a94b248bba8b98
Raymond Usbal <raymond@philippinedev.com>
2020-09-29 13:35:26 +0800

    tmp redirect code

--- app/controllers/crawler_controller.rb
+++ app/controllers/crawler_controller.rb
@@ -1,2 +1,17 @@
 class CrawlerController < ApplicationController
+  def create
+    if process_crawl
+      redirect_to request_urls_url, notice: "Successfully crawled"
+    else
+      redirect_to request_urls_url, alert: "Failed crawl attempt"
+    end
+  end
+
+  private
+
+  def process_crawl
+    true
+  rescue
+    false
+  end
 end

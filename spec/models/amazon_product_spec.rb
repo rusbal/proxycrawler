dddb30ba44ce104671fb86131efb6aaa3ca411b3
Raymond Usbal <raymond@philippinedev.com>
2020-09-30 00:41:25 +0800

    remove old test

--- spec/models/amazon_product_spec.rb
+++ spec/models/amazon_product_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe AmazonProduct, type: :model do
   describe "validations" do
     it { is_expected.to validate_presence_of :amazon_asin }
     it { is_expected.to validate_presence_of :name }
-    it { is_expected.to validate_presence_of :price }
 
     it "validates rating to range 1..5" do
       expect{ create(:amazon_product, rating: 0.9) }.to raise_error(ActiveRecord::RecordInvalid)

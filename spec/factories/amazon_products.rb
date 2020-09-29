eafd5eb86b4e3e0efc2ff7ab8bdab7b33886a204
Raymond Usbal <raymond@philippinedev.com>
2020-09-29 21:48:35 +0800

    improved amazon_product factory

--- spec/factories/amazon_products.rb
+++ spec/factories/amazon_products.rb
@@ -1,13 +1,12 @@
 FactoryBot.define do
   factory :amazon_product do
-    amazon_asin { "B004GIO0F8" }
-    image_url { "MyString" }
-    name { "MyString" }
-    rating { "9.99" }
-    review_count { 1 }
-    price_symbol { "MyString" }
+    amazon_asin { rand(1000000000..9999999999).to_s }
+    sequence(:image_url) { |n| "http://www.example.org/image.#{n}.jpg" }
+    name { "Product name" }
+    rating { rand(1.0..5.0).round(1) }
+    review_count { rand(1..5) }
+    price_symbol { "$" }
     price { "9.99" }
-    price_crossed_out { "9.99" }
-    size_base { "MyString" }
+    price_crossed_out { "19.99" }
   end
 end

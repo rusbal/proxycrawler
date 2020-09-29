5bd665c65a69b0aa7eb0596caf9684413b40ae6f
Raymond Usbal <raymond@philippinedev.com>
2020-09-29 16:02:26 +0800

    handle errors

--- app/controllers/crawler_controller.rb
+++ app/controllers/crawler_controller.rb
@@ -3,15 +3,22 @@ class CrawlerController < ApplicationController
     if process_crawl
       redirect_to request_urls_url, notice: "Successfully crawled"
     else
-      redirect_to request_urls_url, alert: "Failed crawl attempt"
+      redirect_to request_urls_url, alert: @error
     end
   end
 
   private
 
   def process_crawl
-    ProxyCrawler.call
+    request_url = ProxyCrawlRequestUrl.find(params[:request_url_id])
+    ProxyCrawler.call(request_url)
+
+  rescue ActiveRecord::RecordNotFound
+    @error = "Request URL not found"
+    false
+
   rescue
+    @error = "Unhandled error"
     false
   end
 end

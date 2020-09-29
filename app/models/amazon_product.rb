8a925d12ac0a6391db2dc9182903f3e8853f99cf
Raymond Usbal <raymond@philippinedev.com>
2020-09-29 21:48:14 +0800

    validate amazon_asin as unique. rating from 1..5

--- app/models/amazon_product.rb
+++ app/models/amazon_product.rb
@@ -1,5 +1,6 @@
 class AmazonProduct < ApplicationRecord
-  validates :amazon_asin, presence: true
+  validates :amazon_asin, presence: true, uniqueness: true
   validates :name, presence: true
   validates :price, presence: true
+  validates :rating, numericality: { greater_than_or_equal_to: 1.0, less_than_or_equal_to: 5.0 }
 end

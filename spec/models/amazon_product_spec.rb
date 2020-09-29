2306b38e2c9dbba1745d4af86b970a24108d8182
Raymond Usbal <raymond@philippinedev.com>
2020-09-29 21:48:20 +0800

    validate amazon_asin as unique. rating from 1..5

--- spec/models/amazon_product_spec.rb
+++ spec/models/amazon_product_spec.rb
@@ -5,5 +5,15 @@ RSpec.describe AmazonProduct, type: :model do
     it { is_expected.to validate_presence_of :amazon_asin }
     it { is_expected.to validate_presence_of :name }
     it { is_expected.to validate_presence_of :price }
+
+    it "validates rating to range 1..5" do
+      expect{ create(:amazon_product, rating: 0.9) }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+
+    it "validates uniqueness of amazon_asin" do
+      amazon_asin = "1234567890"
+      create(:amazon_product, amazon_asin: amazon_asin)
+      expect{ create(:amazon_product, amazon_asin: amazon_asin) }.to raise_error(ActiveRecord::RecordInvalid)
+    end
   end
 end

6f4ff27290f5be37608aa15e6ec6dd9f93c8e33e
Raymond Usbal <raymond@philippinedev.com>
2020-09-30 00:40:08 +0800

    updated products migration

--- db/migrate/20200929082238_create_amazon_products.rb
+++ db/migrate/20200929082238_create_amazon_products.rb
@@ -4,10 +4,9 @@ class CreateAmazonProducts < ActiveRecord::Migration[6.0]
       t.string :amazon_asin, null: false
       t.string :name, null: false
       t.string :price_symbol
-      t.decimal :price, precision: 8, scale: 2, null: false
+      t.decimal :price, precision: 8, scale: 2
       t.decimal :price_crossed_out, precision: 8, scale: 2
       t.string :image_url
-      t.string :size_base
       t.decimal :rating, precision: 3, scale: 2
       t.integer :review_count, default: 0
 
